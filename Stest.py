b88dbd0e01b7bc4fac163156257a70e397044904
LelushL <117272050+LelushL@users.noreply.github.com>
2022-11-30 11:06:07 +0300

    first commit

---
+++ Stest.py
@@ -0,0 +1,10 @@
+x1 = int(input('Первая клетка \nx1 = '))
+y1 = int(input('y1 = '))
+
+x2 = int(input('Вторая клетка \nx2 = '))
+y2 = int(input('y2 = '))
+
+if x1 == x2 or y1 == y2:
+	print('YES!!!')
+else:
+	ptint('No')
